1a32285370bd73760b2aad96918cc4cccd4de331
sahilrara <shashiparkashrara@gmail.com>
2022-06-27 12:17:30 +0530

    routes done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./index.css";
-import Header from "./components/Header";
-import Financial from "./components/Financial";
-import Benefits from "./components/Benefits";
-import Choose from "./components/Choose";
-import Velittempor from "./components/Velittempor";
-import Testimonials from "./components/Testimonials";
-import Faqs from "./components/Faqs";
-import Newsletter from "./components/Newsletter";
-import Footer from "./components/Footer";
+import { Route, Switch } from "react-router-dom";
+
+import Main from "./view/Main";
+import TermsCondition from "./view/TermsCondition";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Financial />
-      <Benefits />
-      <Choose />
-      <Velittempor />
-      <Testimonials />
-      <Faqs />
-      <Newsletter />
-      <Footer />
+      {" "}
+      <Switch>
+        {" "}
+        <Route exact path="/" component={Main} />
+        <Route exact path="/terms-condition" component={TermsCondition} />
+      </Switch>
     </div>
   );
 }
---
+++ src/view/Main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "../components/Header";
+import Financial from "../components/Financial";
+import Benefits from "../components/Benefits";
+import Choose from "../components/Choose";
+import Velittempor from "../components/Velittempor";
+import Testimonials from "../components/Testimonials";
+import Faqs from "../components/Faqs";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      {" "}
+      <Header />
+      <Financial />
+      <Benefits />
+      <Choose />
+      <Velittempor />
+      <Testimonials />
+      <Faqs />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
