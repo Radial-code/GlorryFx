30c1bd5b85f33a7131411466e92387d165856003
sahilrara <shashiparkashrara@gmail.com>
2022-06-27 14:14:54 +0530

    scrool bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import { useEffect } from "react";
 import "./index.css";
 import { Route, Switch } from "react-router-dom";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Main from "./view/Main";
 import TermsCondition from "./view/TermsCondition";
 import BackToTop from "./components/BackToTop";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
   return (
     <>
       <Switch>
--- src/view/TermsCondition.jsx
+++ src/view/TermsCondition.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 
 const TermsCondition = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Hero />
