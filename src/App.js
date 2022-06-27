0d8b0bad71827f56309b6d2ce59ff47b0a2dfdd6
sahilrara <shashiparkashrara@gmail.com>
2022-06-27 14:00:00 +0530

    backtotop

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Route, Switch } from "react-router-dom";
 
 import Main from "./view/Main";
 import TermsCondition from "./view/TermsCondition";
+import BackToTop from "./components/BackToTop";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/terms-condition" component={TermsCondition} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/terms-condition" component={TermsCondition} />
+      </Switch>
+      <BackToTop />
+    </>
   );
 }
 
